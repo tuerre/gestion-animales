a638fe9fdafb6edf8f64478cf840dd5cffeb19cf
tuerre <jendryjhancel@gmail.com>
2025-09-24 14:26:48 -0400

    AUCH

--- gestionAnimales-backend/src/controllers/usersController.ts
+++ gestionAnimales-backend/src/controllers/usersController.ts
@@ -5,11 +5,10 @@ export const getUserByUsername = async (req, res) => {
         const { username } = req.params;
         const { data, error } = await supabase
         .from("users")
-        .select("*")
+        .select("id, username, name, email, avatar_url")
         .eq("username", username)
         .single();
         if (!data) return res.status(404).json({ error: "Usuario no encontrado" });
-        if (error) return res.status(400).json({ error: error.message });
     res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: "Error interno del servidor" });
