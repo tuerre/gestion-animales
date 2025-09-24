8e96608261bda37785c35d7f2e5644eb8d4ea639
tuerre <jendryjhancel@gmail.com>
2025-09-24 14:18:06 -0400

    refactor and feat: REFACTOR(agregar error personalizado para el caso de no exister el perfil del usuario solicitado) FEAT(Add user profile search by username and not by publicUserId)

--- gestionAnimales-backend/src/controllers/usersController.ts
+++ gestionAnimales-backend/src/controllers/usersController.ts
@@ -1,13 +1,14 @@
 import { supabase } from "../supabase-client.ts";
 
-export const getUserById = async (req, res) => {
+export const getUserByUsername = async (req, res) => {
     try {
-        const { id } = req.params;
+        const { username } = req.params;
         const { data, error } = await supabase
         .from("users")
         .select("*")
-        .eq("id", id)
+        .eq("username", username)
         .single();
+        if (!data) return res.status(404).json({ error: "Usuario no encontrado" });
         if (error) return res.status(400).json({ error: error.message });
     res.status(200).json(data);
     } catch (error) {
