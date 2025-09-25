cd2faeb3fdd8deb92c4b15aabca60db497cdfe4c
tuerre <jendryjhancel@gmail.com>
2025-09-25 19:01:50 -0400

    frontend default pages and routes

--- gestionAnimales-backend/src/routes/users.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getUserByUsername } from "../controllers/usersController.ts";
-
-const router = Router();
-
-router.get("/:username", getUserByUsername);
-
-export default router;
--- gestionAnimales-backend/src/supabase-client.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import { createClient, processLock } from "@supabase/supabase-js";
-
-dotenv.config();
-
-export const supabase = createClient(
-  process.env.PROJECT_URL as string,
-  process.env.ANON_KEY as string
-);
