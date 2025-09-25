f5a85e724d4c2f9e4a030f332e56f736e57abbc7
tuerre <jendryjhancel@gmail.com>
2025-09-25 18:51:54 -0400

    feat: Installing Astro and Tailwind and their default settings

---
+++ backend/src/index.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import cors from "cors";
+import express from "express";
+
+import usersRoutes from "./routes/users.ts";
+
+// routes
+import authRoutes from "./routes/auth.ts";
+import animalsRoutes from "./routes/animals.ts";
+
+dotenv.config();
+
+const app = express();
+
+// middlewares
+app.use(express.json());  
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:4321',
+  credentials: true,
+}));
+
+app.use("/auth", authRoutes);
+app.use("/animals", animalsRoutes);
+app.use("/users", usersRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("the API is working!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${process.env.PORT}`);
+});
---
+++ frontend/astro.config.mjs
@@ -0,0 +1,12 @@
+import react from '@astrojs/react';
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [react()],
+});
